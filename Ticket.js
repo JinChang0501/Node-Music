8f9123a1beb42c04ba38e68e31fa770de6190390
Efron <a161026521@gmail.com>
2024-07-05 16:49:03 +0800

    first

---
+++ Ticket.js
@@ -0,0 +1,58 @@
+import { DataTypes } from 'sequelize'
+
+export default async function (sequelize) {
+  return sequelize.define(
+    'Ticket',
+    {
+      tid: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      activities_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Activity',
+          key: 'actid',
+        },
+      },
+      seat_area: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      seat_row: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      seat_number: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      members_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Member',
+          key: 'id',
+        },
+      },
+      order_num: {
+        type: DataTypes.STRING(50),
+        allowNull: true,
+      },
+    },
+    {
+      tableName: 'ticket',
+      timestamps: true, // 使用時間戳
+      paranoid: false, // 軟性刪除
+      underscored: true, // 所有自動建立欄位，使用snake_case命名
+      createdAt: 'created_at', // 建立的時間戳
+      updatedAt: 'updated_at', // 更新的時間戳
+    }
+  )
+}
