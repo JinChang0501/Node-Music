1b20a141698780afe133f5cfb975f47e5526efff
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-10 00:55:51 +0800

    更改 google-login,js 參考資料表

--- routes/google-login.js
+++ routes/google-login.js
@@ -2,7 +2,7 @@ import express from 'express'
 const router = express.Router()
 
 import sequelize from '#configs/db.js'
-const { User } = sequelize.models
+const { Member } = sequelize.models
 
 import jsonwebtoken from 'jsonwebtoken'
 // 存取`.env`設定檔案使用
@@ -29,7 +29,7 @@ router.post('/', async function (req, res, next) {
   // 2-2. 不存在 -> 建立一個新會員資料(無帳號與密碼)，只有google來的資料 -> 執行登入工作
 
   // 1. 先查詢資料庫是否有同google_uid的資料
-  const total = await User.count({
+  const total = await Member.count({
     where: {
       google_uid,
     },
@@ -39,14 +39,14 @@ router.post('/', async function (req, res, next) {
   // 存取令牌(access token)只需要id和username就足夠，其它資料可以再向資料庫查詢
   let returnUser = {
     id: 0,
-    username: '',
+    name: '',
     google_uid: '',
-    line_uid: '',
+    // line_uid: '',
   }
 
   if (total) {
     // 2-1. 有存在 -> 從資料庫查詢會員資料
-    const dbUser = await User.findOne({
+    const dbUser = await Member.findOne({
       where: {
         google_uid,
       },
@@ -56,9 +56,9 @@ router.post('/', async function (req, res, next) {
     // 回傳給前端的資料
     returnUser = {
       id: dbUser.id,
-      username: dbUser.username,
+      name: dbUser.name,
       google_uid: dbUser.google_uid,
-      line_uid: dbUser.line_uid,
+      // line_uid: dbUser.line_uid,
     }
   } else {
     // 2-2. 不存在 -> 建立一個新會員資料(無帳號與密碼)，只有google來的資料 -> 執行登入工作
@@ -70,14 +70,14 @@ router.post('/', async function (req, res, next) {
     }
 
     // 新增會員資料
-    const newUser = await User.create(user)
+    const newUser = await Member.create(user)
 
     // 回傳給前端的資料
     returnUser = {
       id: newUser.id,
-      username: '',
+      name: '',
       google_uid: newUser.google_uid,
-      line_uid: newUser.line_uid,
+      // line_uid: newUser.line_uid,
     }
   }
 
