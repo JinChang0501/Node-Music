d37d58e0f07530fe6221f58594b9a1884772ddcc
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-15 20:13:58 +0800

    後端越來越好玩了

---
+++ routes/store-order.js
@@ -0,0 +1,34 @@
+import express from 'express'
+const router = express.Router()
+
+// 中介軟體，存取隱私會員資料用
+import authenticate from '#middlewares/authenticate.js'
+
+// 檢查空物件, 轉換req.params為數字
+import { getIdParam } from '#db-helpers/db-tool.js'
+
+// 資料庫使用
+import db from '../utils/connect-mysql.js'
+
+// GET - 得到所有Ticket資料
+// router.get('/', authenticate, async function (req, res) {
+//   const sql = `SELECT * FROM ticket`
+
+//   const [result] = await db.query(sql)
+//   res.json({ result })
+//   // 處理如果沒找到資料
+// })
+
+//GET - 得到所有登入之會員的購物紀錄
+router.get('/', authenticate, async function (req, res) {
+  const id = +req.user.id
+
+  const sql = `SELECT a.order_num FROM order_detail as a join product as b on a.product_id = b.id WHERE a.member_id = ${id} group by a.order_num;`
+
+  const [result] = await db.query(sql)
+  // res.json({ result })
+  return res.json({ status: 'success', data: { result } })
+  // 處理如果沒找到資料
+})
+
+export default router
