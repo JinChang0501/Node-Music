9864bc3a15eded4f07890580bf718f2d3fd7b15c
Castle9707 <paomincc25@gmail.com>
2024-07-17 15:52:31 +0800

    收藏Done，可成功加入&刪除

--- routes/favorite.js
+++ routes/favorite.js
@@ -6,20 +6,22 @@ import { getIdParam } from '#db-helpers/db-tool.js'
 
 // 登入驗證（不確定我們是用這個嗎）
 import authenticate from '#middlewares/authenticate.js'
-// 連接資料庫
+// 連接 MySQL 資料庫
 import db from "../utils/connect-mysql.js"
 
 // 獲得某會員id的 有加入到我的最愛清單中的商品id們
 // 此路由只有登入會員能使用 authenticate
 
 // 加入收藏
-router.post('/:userId', authenticate, async (req, res) => {
-  const { userId, eventId } = req.body
+router.post('/', authenticate, async (req, res) => {
+  const { eventId } = req.body
+  const id = +req.user.id
+
   const p_sql = `INSERT INTO \`favorite\` (member_id, item_id) VALUES (?, ?)`
 
   try {
-    await db.query(p_sql, [userId, eventId])
-    res.status(201).json({ message: '收藏成功' })
+    await db.query(p_sql, [id, eventId])
+    res.status(201).json({ message: '收藏成功', eventId: {eventId} })
   } catch (error) {
     res.status(500).json({ error: '無法加入收藏' })
   }
@@ -27,12 +29,13 @@ router.post('/:userId', authenticate, async (req, res) => {
 
 // 取消收藏
 router.delete('/', authenticate, async (req, res) => {
-  const { userId, eventId } = req.body
+  const { eventId } = req.body
+  const id = +req.user.id
   const d_sql = `DELETE FROM \`favorite\` WHERE member_id = ? AND item_id = ?`
 
   try {
-    await db.query(d_sql, [userId, eventId])
-    res.status(200).json({ message: '取消收藏成功' })
+    await db.query(d_sql, [id, eventId])
+    res.status(200).json({ message: '取消收藏成功', eventId: {eventId} })
   } catch (error) {
     res.status(500).json({ error: '無法取消收藏' })
   }
@@ -40,14 +43,28 @@ router.delete('/', authenticate, async (req, res) => {
 
 // 獲取使用者收藏的活動
 // 測試先刪 authenticate，測試完記得補上登入驗證
-router.get('/:userId', async (req, res) => {
-  const { userId } = req.params
-  // const id = +req.user.id
-  const sql = `SELECT * FROM \`favorite\` WHERE member_id = ?`
+// router.get('/:userId', async (req, res) => {
+//   const { userId } = req.params
+//   // const id = +req.user.id
+//   const sql = `SELECT * FROM \`favorite\` WHERE member_id = ?`
+
+//   try {
+//     const [results] = await db.query(sql, [userId])
+//     res.status(200).json(results)
+//   } catch (error) {
+//     res.status(500).json({ error: '無法獲取收藏' })
+//   }
+// })
+
+router.get('/', authenticate, async (req, res) => {
+  // const { userId } = req.params
+  const id = +req.user.id
+  const sql = `SELECT item_id FROM \`favorite\` WHERE member_id = ?`
 
   try {
-    const [results] = await db.query(sql, [userId])
-    res.status(200).json(results)
+    const [rows] = await db.query(sql, id)
+    const favorites = rows.map(row => row.item_id)
+    res.status(200).json({ status: 'success', rows: { favorites } })
   } catch (error) {
     res.status(500).json({ error: '無法獲取收藏' })
   }
@@ -55,11 +72,12 @@ router.get('/:userId', async (req, res) => {
 
 // 確認是否收藏
 router.get('/check', async (req, res) => {
-  const { userId, eventId } = req.query
+  const { eventId } = req.query
+  const id = +req.user.id
   const c_sql = `SELECT * FROM favorites WHERE member_id = ? AND item_id = ?`
 
   try {
-    const [results] = await db.query(c_sql, [userId, eventId])
+    const [rows] = await db.query(c_sql, [id, eventId])
     if (results.length > 0) {
       res.status(200).json({ isFavorite: true })
     } else {
