eeb535f2188c3941702b192a3e5f37264e80bcd9
JinChang0501 <165128304+JinChang0501@users.noreply.github.com>
2024-07-22 17:04:54 +0800

    update

---
+++ routes/memberPic.js
@@ -0,0 +1,25 @@
+import express from 'express'
+const router = express.Router()
+
+// 中介軟體，存取隱私會員資料用
+import authenticate from '#middlewares/authenticate.js'
+
+// 檢查空物件, 轉換req.params為數字
+import { getIdParam } from '#db-helpers/db-tool.js'
+
+// 資料庫使用
+import db from '../utils/connect-mysql.js'
+
+// GET - 得到登入會員的資料
+router.get('/', authenticate, async function (req, res) {
+  const id = +req.user.id
+
+  const sql = `SELECT * FROM member where id = ${id}`
+
+  const [result] = await db.query(sql)
+  // res.json({ result })
+  return res.json({ status: 'success', data: { result } })
+  // 處理如果沒找到資料
+})
+
+export default router
